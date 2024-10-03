386f62857cb0356ae084986782e89c6ea059a694
wondrous05 <oagold264@gmail.com>
2024-10-03 07:38:34 -0700

    cors

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ const app = express()
 const signup = require("../src/routes/signup.routes")
 
 app.use(morgan("dev"))
-app.use(cors())
+
+app.use(cors({
+  origin:"*",
+  methods:["GET","POST","DELETE","PUT","PATCH"],
+  credentials:true
+}))
+
 app.use(express.json())
 app.use(session({ secret: 'your secret', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
